b13d9d45ab1f4b2ff93d8f5de0ab6eba10690d20
jackbouncy7 <otaboevamadina@gmail.com>
2024-11-02 19:51:08 +0500

    Improved whole site, add interactions, change responsiveness

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -29,55 +29,62 @@ const Products = () => {
 
     fetchProducts();
   }, []);
+  console.log(products);
 
   return (
     <>
       <Header />
       <section className="section products">
         <div className="container">
-          <h2 className="products__title">Featured case studies</h2>
+          <h2 className="products__title">Our products</h2>
 
-          {loading ? (
-            <Skeleton
-              height={568}
-              width={"100%"}
-              borderRadius={32}
-              baseColor="#ddd"
-              highlightColor="#f3f3f3"
-            />
-          ) : (
-            products.map((product, index) => (
-              <Link key={product.id} to={`/products/${product.id}`}>
-                <div
-                  className="products__box"
-                  style={{
-                    backgroundImage: `url(${product.imgList[0]?.img})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "right",
-                  }}
-                >
-                  <div className="products__content">
-                    <h3 className="products__title--content">
-                      {product?.name}
-                    </h3>
-                    <span className="products__devices">
-                      {product.tags.join(", ")}
-                    </span>
-                    {/* <span className='products__category'>Finance</span> */}
-                    {/* <span className='products__country'>US</span> */}
-                    <div className="products__button-link">
-                      <Link
-                        className="products__link"
-                        to={`/products/${product.id}`}
-                      >
-                        Explore more <i className="fa fa-arrow-right"></i>{" "}
-                      </Link>
+          <div className="product__grid">
+            {loading ? (
+              <Skeleton
+                height={568}
+                width={"100%"}
+                borderRadius={32}
+                baseColor="#ddd"
+                highlightColor="#f3f3f3"
+              />
+            ) : (
+              products.map((product, index) => (
+                <Link key={product.id} to={`/products/${product.id}`}>
+                  <div
+                    className="products__box"
+                    style={{
+                      backgroundImage: `url(${product.imgList[0]?.img})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "right",
+                    }}
+                  >
+                    <div className="products__content">
+                      <h3 className="products__title--content">
+                        {product?.name}
+                      </h3>
+                      <span className="products__devices">
+                        {product.tags.map((tag, index) => (
+                          <span key={index} className="products__device-tag">
+                            {tag}
+                          </span>
+                        ))}
+                      </span>
+                      {/* <span className='products__category'>Finance</span> */}
+                      {/* <span className='products__country'>US</span> */}
+                      <div className="products__button-link">
+                        <Link
+                          className="products__link"
+                          to={`/products/${product.id}`}
+                        >
+                          Explore more <i className="fa fa-arrow-right"></i>{" "}
+                        </Link>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Link>
-            ))
-          )}
+                </Link>
+              ))
+            )}
+          </div>
         </div>
       </section>
       <Footer />
