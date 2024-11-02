2ef1fb140634980a4598a60ac741decdcae068aa
jackbouncy7 <otaboevamadina@gmail.com>
2024-11-02 22:47:59 +0500

    Improved whole site, add interactions, change responsiveness

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -40,71 +40,74 @@ const Products = () => {
           <h2 className="products__title">Our products</h2>
 
           <div className="product__grid">
-            {loading ? (
-              <Skeleton
-                height={568}
-                width={"100%"}
-                borderRadius={32}
-                baseColor="#ddd"
-                highlightColor="#f3f3f3"
-              />
-            ) : (
-              products.map((product, index) => (
-                <Link key={product.id} to={`/products/${product.id}`}>
-                  <div
-                    className="products__box"
-                    style={{
-                      backgroundImage: `url(${product.imgList[0]?.img})`,
-                      backgroundSize: "cover",
-                      backgroundPosition: "right",
-                    }}
-                  >
-                    <div className="products__content">
-                      <h3 className="products__title--content">
-                        {product?.name}
-                      </h3>
-                      <span className="products__devices">
-                        {product.tags.map((tag, index) => (
-                          <span key={index} className="products__device-tag">
-                            {tag}
-                          </span>
-                        ))}
-                      </span>
-                      <div className="product__downloads">
-                        {product?.appstore && (
-                          <a href={product?.appstore}>
-                            <img
-                              src={AppStoreLogo}
-                              width={120}
-                              height={40}
-                              alt="product download link"
-                            />
-                          </a>
-                        )}
-                        {product?.playmarket && (
-                          <a href={product?.playmarket}>
-                            <img
-                              src={PlayStoreLogo}
-                              width={120}
-                              height={40}
-                              alt="product download link"
-                            />
-                          </a>
-                        )}
-                      </div>
-                      <div className="products__button-link">
-                        <Link
-                          className="products__link"
-                          to={`/products/${product.id}`}
-                        >
-                          Explore more <i className="fa fa-arrow-right"></i>{" "}
-                        </Link>
+            {loading
+              ? Array(6)
+                  .fill()
+                  .map((_, index) => (
+                    <Skeleton
+                      key={index}
+                      height={568}
+                      width={"100%"}
+                      borderRadius={32}
+                      baseColor="#ddd"
+                      highlightColor="#f3f3f3"
+                    />
+                  ))
+              : products.map((product, index) => (
+                  <Link key={product.id} to={`/products/${product.id}`}>
+                    <div
+                      className="products__box"
+                      style={{
+                        backgroundImage: `url(${product.imgList[0]?.img})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "right",
+                      }}
+                    >
+                      <div className="products__content">
+                        <h3 className="products__title--content">
+                          {product?.name}
+                        </h3>
+                        <span className="products__devices">
+                          {product.tags.map((tag, index) => (
+                            <span key={index} className="products__device-tag">
+                              {tag}
+                            </span>
+                          ))}
+                        </span>
+                        <div className="product__downloads">
+                          {product?.appstore && (
+                            <a href={product?.appstore}>
+                              <img
+                                src={AppStoreLogo}
+                                width={120}
+                                height={40}
+                                alt="product download link"
+                              />
+                            </a>
+                          )}
+                          {product?.playmarket && (
+                            <a href={product?.playmarket}>
+                              <img
+                                src={PlayStoreLogo}
+                                width={120}
+                                height={40}
+                                alt="product download link"
+                              />
+                            </a>
+                          )}
+                        </div>
+                        <div className="products__button-link">
+                          <Link
+                            className="products__link"
+                            to={`/products/${product.id}`}
+                          >
+                            Explore more <i className="fa fa-arrow-right"></i>{" "}
+                          </Link>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </Link>
-              ))
-            )}
+                  </Link>
+                ))}
           </div>
         </div>
       </section>
