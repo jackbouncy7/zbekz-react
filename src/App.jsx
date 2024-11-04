8244a779aa7a38d7c9d4672bc3b8cc36f8b18da8
jackbouncy7 <otaboevamadina@gmail.com>
2024-11-04 16:17:25 +0500

    Add redirects file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { React, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
+import ScrollToTop from "@components/ScrollToTop/ScrollToTop";
 import { Public } from "./pages/Public/Public";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route path="/*" element={<Public />} />
       </Routes>
