433f328c4d24748688d5b0d8d0bc90d19849e9e7
jackbouncy7 <otaboevamadina@gmail.com>
2024-11-06 22:59:01 +0500

    Get all the data components

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -2,7 +2,7 @@ import { lazy } from "react";
 const HeroParallax = lazy(() =>
   import("@components/HeroParallax/HeroParallax")
 );
-import CallToAction from "@components/CalltoAction/CallToAction";
+import CallToAction from "@components/CalltoAction/CalltoAction";
 const Footer = lazy(() => import("@components/Footer/Footer"));
 import "./_services.scss";
 
