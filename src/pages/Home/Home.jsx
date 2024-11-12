4b86ec989b42818cec7b4c7ef38630ee0d8b5a39
jackbouncy7 <otaboevamadina@gmail.com>
2024-11-12 14:43:27 +0500

    Add product icons to homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,7 +11,7 @@ import img2png from "@images/inomarket_logo.svg";
 import img3png from "@images/karly_icon.svg";
 import img4png from "@images/quranic_calm.svg";
 import img5png from "@images/tashkent_metro.svg";
-import img6png from "@images/6.png";
+import img6png from "@images/innobot.png";
 // import WelcomeBg from "@images/welcome.jpg";
 import HomeMockup from "@images/home-mockup.png";
 // import Blog1 from "@images/1_1.jpg";
@@ -55,7 +55,7 @@ const Home = () => {
                 <img src={img5png} alt="Tashkent Metro product Zbekz icon" />
               </a>
               <a href="/products">
-                <img src={img6png} alt="App Icon 6" />
+                <img src={img6png} alt="Inno bot product Zbekz icon" />
               </a>
             </div>
           </div>
